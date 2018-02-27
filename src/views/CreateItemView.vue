44cd016a4ae1b85e05fc61a8550512e82b2c25c5
xernyuii <hfutxc.xern@gmail.com>
2018-02-28 01:24:55 +0800

    update create token page

--- src/views/CreateItemView.vue
+++ src/views/CreateItemView.vue
@@ -1,72 +1,68 @@
 <template>
   <div class="createTokenView">
+    <div class="hero-body">
+      <div class="column is-4 is-offset-4">
+        <h3 class="title has-text-grey has-text-centered">Create Token</h3>
+        <p class="subtitle has-text-grey has-text-centered">You can create your own token now</p>
+        <div class="container">
+          <div class="box" style="width: 400px;">
+            <div class="field is-horizen">
+              <label class="label">发行价</label>
+              <div class="field ">
+                <p class="control">
+                    <input class="input"
+                      type="number"
+                      v-model="price"
+                      placeholder="输入Token的发行价">
+                </p>
+              </div>
+              <label class="label">Frozen时间</label>
+              <div class="field ">
+                <p class="control">
+                  <input class="input"
+                        type="number"
+                        v-model="frozen1"
+                        placeholder="请输入冻结的时间">
+                </p>
+              </div>
 
-    <label class="label">发行价</label>
-    <div class="field has-addons">
-      <p class="control">
-        <input class="input"
-               type="number"
-               v-model="price"
-               placeholder="输入Token的发行价">
+              <!--<p class="help">This username is available</p>-->
+              <label class="label">父Token ID</label>
+              <div class="field ">
+                <p class="control">
+                  <input class="input"
+                        type="number"
+                        v-model="ftoken"
+                        placeholder="请输入父token的编号">
+                </p>
+              </div>
 
-      </p>
-      <p class="control">
-        <a class="button is-static">
-          ETH
-        </a>
-      </p>
-    </div>
+              <label class="label">Token 名称</label>
+              <div class="field ">
+                <p class="control">
+                  <input class="input"
+                        type="text"
+                        v-model="Tname"
+                        placeholder="请输入Token的名称">
+                </p>
+              </div>
 
-    <label class="label">Frozen1时间</label>
-    <div class="field has-addons">
-      <p class="control">
-        <input class="input"
-               type="number"
-               v-model="frozen1"
-               placeholder="">
-
-      </p>
-      <p class="control">
-        <a class="button is-static">
-          秒
-        </a>
-      </p>
-    </div>
-
-    <label class="label">Frozen2时间</label>
-    <div class="field has-addons">
-      <p class="control">
-        <input class="input"
-               type="number"
-               v-model="frozen2"
-               placeholder="">
-
-      </p>
-      <p class="control">
-        <a class="button is-static">
-          秒
-        </a>
-      </p>
-    </div>
-    <!--<p class="help">This username is available</p>-->
-
-    <div class="field">
-      <label class="label">父Token编号</label>
-      <div class="control">
-        <input class="input"
-               type="number"
-               v-model="parentId"
-               placeholder="">
-      </div>
-    </div>
-
-    <div class="field is-grouped">
-      <div class="control">
-        <button class="button is-link"
-                @click="onSubmit">确认</button>
-      </div>
-      <div class="control">
-        <button class="button is-text">取消</button>
+              <label class="label">Token 简介</label>
+              <div class="field ">
+                <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
+              </div>
+              <div class="field is-grouped">
+                <div class="control">
+                  <button class="button is-link is-fullwidth"
+                          @click="onSubmit">确认</button>
+                </div>
+                <div class="control">
+                  <button class="button is-text">取消</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
