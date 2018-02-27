c05a07e8fc325006eeb9b114f2ffc94ee5fe1522
xernyuii <hfutxc.xern@gmail.com>
2018-02-28 03:26:54 +0800

    new main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,116 @@
 <template>
-  <div>
-    Home page
+<section class="primary-background" style="background-image: url('/static/icons-background.svg')">
+    <section class="section has-text-centered">
+        <div class="section">
+            <h1 class="title has-text-grey" style="font-size: 3rem;">
+                Smart Signature, next generation of token ID.
+            </h1>
+            <h1 class="sub titlehas-text-grey">
+                Now you can design your own Signature.
+            </h1>
+        </div>
+    </section>
+    <div class="box cta">
+    <p class="has-text-centered">
+      <span class="tag is-primary">New</span> Smart Signature version @beta 0.1 now is online. You can test the new features of creating tokens for yourself. 
+    </p>
   </div>
+  <div class="container">
+        <h2 class="section title has-text-centered">About Smart Signature</h2>
+        <div class="section">
+            <div class="columns">
+                <div class="column feature-box">
+                    <h2 class="subtitle">
+                        What's Smart Signature ?
+                    </h2>
+                    <div class="text">
+                        智能签名是通过以太坊钱包和私钥，使用智能合约在以太坊区块链上签发的符合一定标准的 ERC721 通证。
+                    </div>
+                    <div class="text">
+                        智能签名可以在帮助你验证身份的同时，完成诸如，智能打赏，赞助，竞拍，营销，等特定的功能。
+                    </div>
+                </div>
+                <div class="column">
+                    <img class="image-box border-shadow " src="/static/img/eth.png" alt="">
+                </div>
+            </div>
+        </div>
+  </div>
+<!-- para -->
+  <section class="container">
+
+    <div class="columns features">
+      <div class="column is-4">
+        <h3 class="title is-5 feature-title has-text-black has-text-centered has-text-weight-normal">
+           Bring back your ownership<br>
+           through blockchain.
+        </h3>
+
+        <div class="box" id="business-expertise">
+          <div class="card-content">
+            <img class="image-box border-shadow " src="/static/img/" alt="">
+            <p>
+              <!-- <span class="key-question">Pergunta chave:</span> -->
+              <br>
+              </p><h4 class="title is-4 has-text-weight-normal key-question">
+                完璧归赵，通过区块链技术从大公司的手中取回你的著作权。
+              </h4>
+            <p></p>
+            <p class="feature-description">
+              我们原来常常说，对 Google、Facebook 等大公司而言，用户实际上并不是客户，而是它的产品，广告商才是它们真正的客户。通过使用智能签名技术，可以有效防止内容产生者所产生的劳动价值被平台剥削。
+            </p>
+          </div>
+        </div>
+      </div><!-- direcionamento de produto -->
+      <div class="column is-4">
+        <h3 class="title is-5 feature-title has-text-black has-text-centered has-text-weight-normal">
+          Design de<br>
+          serviços (UI/UX)
+        </h3>
+        <div class="card is-shadowless is-radiusless" id="design-expertise" style="height: 508px;">
+          <div class="card-content">
+            <i class="material-icons">people</i>
+            <p>
+              <span class="key-question">Pergunta chave:</span>
+              <br>
+              </p><h4 class="title is-4 has-text-weight-normal key-question">
+                A solução atende e desperta o desejo do público alvo?
+              </h4>
+            <p></p>
+            <p class="feature-description">
+              No processo de design é preciso ponderar estética e funcionalidade, para que a experiência proporcionada pela solução <strong>engaje, desperte desejo e atenda as reais necessidades</strong> do negócio e de seus usuários.
+            </p>
+          </div>
+        </div>
+      </div><!-- design de serviços -->
+      <div class="column is-4">
+        <h3 class="title is-5 feature-title has-text-black has-text-centered has-text-weight-normal">
+          Tecnologia e<br>
+          desenvolvimento
+        </h3>
+
+        <div class="card is-shadowless is-radiusless" id="development-expertise" style="height: 508px;">
+          <div class="card-content">
+            <i class="material-icons">phonelink</i>
+            <p>
+              <span class="key-question">Pergunta chave:</span>
+              <br>
+              </p><h4 class="title is-4 has-text-weight-normal key-question">
+                A solução pode ser viabilizada?
+              </h4>
+            <p></p>
+            <p class="feature-description">
+              Toda ferramenta tem um propósito que permite atingir um determinado objetivo.<br>
+              <strong>Temos domínio em diversas tecnologias</strong> e iremos propor a que melhor se encaixar no contexto do seu produto ou serviço.
+            </p>
+          </div>
+        </div>
+      </div><!-- tecnologia e desenvolvimento -->
+    </div>
+
+    <!-- // PRODUTOS -->
+
+  </section>
+</section>
+
 </template>
