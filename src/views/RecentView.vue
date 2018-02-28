a65d4115a6a7b44d72e9f8ad42b8e5841ba01ada
xernyuii <hfutxc.xern@gmail.com>
2018-02-28 23:26:25 +0800

    add recentview

--- src/views/RecentView.vue
+++ src/views/RecentView.vue
@@ -1,12 +1,5 @@
 <template>
-	<tr v-for="(item, index) of slist">
-    <td>{{index+1}}</td>
-    <td>{{item.name}}</td>
-    <td>{{item.owner}}</td>
-    <td>{{item.price}}</td>
-    <td>{{item.time}}</td>
-    <!-- <td><a href="javascript:;" @click="showOverlay(index)">修改</a> | <a href="javascript:;" @click="del(index)">删除</a></td> -->
-	</tr>
+
 
 </template>
 
