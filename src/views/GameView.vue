3ac7ed770cd1b3857c82afd3448298ca8c82ce70
KJlmfe <kjlmfe@gmail.com>
2018-03-02 15:21:49 +0800

    feat(GameView): display game map with gameGeo.json

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,6 +1,42 @@
 <template>
-  <div class="content">
-  	<div id="map" style="height: 100%">
-  	123</div>
+  <div class="game-view">
+    <div class="columns">
+      <div class="column">
+        <ve-map :data="chartData"
+                :settings="chartSettings"
+                :extend="chartExtend"></ve-map>
+      </div>
+      <div class="column">
+        UI
+    </div>
+    </div>
   </div>
 </template>
+
+<script>
+import VeMap from "v-charts/lib/map";
+export default {
+  components: { VeMap },
+
+  created: function() {
+    this.chartData = {
+      columns: ["位置", " 人口"],
+      rows: [{ 位置: "蜀国", " 人口": 123 }]
+    };
+    this.chartSettings = {
+      positionJsonLink: "./static/gameGeo.json",
+      position: "world",
+      beforeRegisterMap(json) {
+        return json;
+      }
+    };
+    this.chartExtend = {
+      series: {
+        nameMap: {
+          SHU: "蜀国"
+        }
+      }
+    };
+  }
+};
+</script>
