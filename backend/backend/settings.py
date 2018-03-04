7724418ae8edae545a30c91b972727361a7dccfb
wangan'gang <wangangang@spriteapp.com>
2018-03-04 11:40:26 +0800

    add allowed_host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["api.lucky917.com"]
 
 
 # Application definition
