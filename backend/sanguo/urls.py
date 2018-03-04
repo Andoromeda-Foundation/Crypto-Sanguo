6b697ab1a75bf74b3fc5a17975b70dad0a283a7c
wangan'gang <wangangang@spriteapp.com>
2018-03-05 00:00:57 +0800

    [backend]login增加额外信息 增加user_info接口

--- backend/sanguo/urls.py
+++ backend/sanguo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from sanguo.views import hero_view, login_view, my_address_view
+from sanguo.views import hero_view, login_view, my_address_view, user_info_view
 
 urlpatterns = [
     path('heroes/', hero_view),
     path('login/', login_view),
     path('my_address/', my_address_view),
+    path('user_info/', user_info_view)
 ]
--- backend/sanguo/views.py
+++ backend/sanguo/views.py
@@ -1,8 +1,9 @@
+import traceback
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.forms.models import model_to_dict
 from sanguo.constants import TOKEN_EXPIRE_AFTER
-from sanguo.models import Heroes
+from sanguo.models import Heroes, HeroOwnership, CityOwnership, UserBattleInfo
 import json
 from django.core import serializers
 
@@ -19,7 +20,55 @@ def login_view(request):
         return JsonResponse({"err_code": 401, "msg": "address为空"})
     request.session['uid'] = address
     request.session.set_expiry(TOKEN_EXPIRE_AFTER)
-    return JsonResponse({"err_code": 0, "err_msg": ""})
+
+    # {
+    #     int id;
+    #     string sessionId;
+    #     string name;
+    #     string address;
+    #     int soldier;
+    #     int heroList[];
+    #     int cityList[];
+    # }
+    player_info = {
+        "session_id": request.session.session_key,
+        "name": address,
+        "address": address,
+    }
+    user_battle_info = get_user_battle_info(address)
+    player_info.update(user_battle_info)
+    return JsonResponse({"err_code": 0, "err_msg": "", "player_info": player_info})
+
+
+def get_user_battle_info(address):
+    """
+    int soldier;
+    int heroList[];
+    int cityList[];
+    """
+    heroes = HeroOwnership.objects.filter(address=address)
+    cities = CityOwnership.objects.filter(address=address)
+    try:
+        user_battle = UserBattleInfo.objects.get(address=address)
+    except Exception as err:
+        user_battle = []
+    hero_list = []
+    city_list = []
+    for hero in heroes:
+        try:
+            hero_list.append(model_to_dict(Heroes.objects.get(pk=hero.hero_id)))
+        except Exception as err:
+            print("err: %s, traceback: %s" % (err, traceback.format_exc()))
+    for city in cities:
+        try:
+            city_list.append(model_to_dict(Heroes.objects.get(pk=city.city_id)))
+        except Exception as err:
+            print("err: %s, traceback: %s" % (err, traceback.format_exc()))
+    return {
+        "heroes": hero_list,
+        "cities": city_list,
+        "soldier": user_battle.soldier if user_battle else 0
+    }
 
 
 def my_address_view(request):
@@ -28,3 +77,9 @@ def my_address_view(request):
         return {"err_code": 401, "msg": "用户未登录 请重新登录"}
 
     return JsonResponse({"err_code": 0, "address": address})
+
+
+def user_info_view(request):
+    address = request.GET.get("address")
+    battle_info = get_user_battle_info(address)
+    return JsonResponse(battle_info)
