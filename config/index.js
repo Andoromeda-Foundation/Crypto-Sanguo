65fb6651f78f5f883d38715d87197d22380197fc
KJlmfe <kjlmfe@gmail.com>
2018-03-16 15:36:45 +0800

    chore(webpack): set assetsPublicPath='/Crypto-Sanguo' for gh-pages

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/sponsor-token-website',
+    assetsPublicPath: '/Crypto-Sanguo',
 
     /**
      * Source Maps
