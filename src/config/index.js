b6f4e1885e1664176e15874859449f6ea1f4985a
gaojin <1019855680@qq.com>
2018-03-31 00:55:03 +0800

    使用kovan 测试网.

--- src/config/index.js
+++ src/config/index.js
@@ -26,7 +26,9 @@ export const network = {
   },
   42: {
     name: 'Kovan Test Network',
-    contract: '',
+    contract: '0xa50b0eb34d3ed582f9e3a691bec970be10065193',
+    LuckyPackage: '0x5fdb566562a41203221a82bd50767fa25ae4edae',
+    DecentralizedExchangeHotPotato: '0xac7e1a3263e06329eee8ac5b45f3ac2223119779',
     rpc: 'https://kovan.infura.io/lTETGFVyQX99UKQ98BN4',
   },
   5: {
@@ -34,7 +36,7 @@ export const network = {
     name: 'Private Network',
     contract: '0x828a929fc03cacf7e5ff09771d8ae9d5ca17344d',
     LuckyPackage: '0x58a84ee22f8e6f371db58393b9d65420cf068573',
-    // DecentralizedExchangeHotPotato: '0xcb5f7224dd08e4d37a350637185b8a1be17359a1',
+    DecentralizedExchangeHotPotato: '0xcb5f7224dd08e4d37a350637185b8a1be17359a1',
     rpc: 'http://127.0.0.1:7546',
   }
 };
