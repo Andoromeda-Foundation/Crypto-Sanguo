e4d5f4a3548710b8f639fe7a578f28d266a997e5
KJlmfe <kjlmfe@gmail.com>
2018-04-06 10:57:58 +0800

    fix(Header): 设置顶部导航栏为透明背景色

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -139,8 +139,8 @@ header {
   position: relative;
 }
 .navbar {
-  background: rgba(0, 0, 0, 0.5);
-  /* position: absolute; */
+  background: none;
+  position: absolute;
   top: 0;
   left: 0;
   right: 0;
