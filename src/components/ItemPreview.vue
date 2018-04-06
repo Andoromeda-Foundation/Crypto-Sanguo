97f7cc174dd97cea3891c0666cea89e58ff25351
KJlmfe <kjlmfe@gmail.com>
2018-04-06 10:50:37 +0800

    perf(ItemPreview): 懒加载卡牌图片

--- src/components/ItemPreview.vue
+++ src/components/ItemPreview.vue
@@ -5,7 +5,7 @@
       <img class="KJ-width-100"
            src="@/assets/img/cardBorder.png" />
       <img class="card-img"
-           :src="item.imageUrl"/>
+           v-lazy="item.imageUrl"/>
     </div>
     <div class="card-fooer columns is-mobile is-gapless has-text-centered has-text-weight-bold">
       <h5 class="column card-name is-4">{{item.title}}
