63e7cd67c1f2e367b591a76e13286b0b3b9f4186
gaojin <1019855680@qq.com>
2018-04-08 00:21:58 +0800

    更正 getPackTxApi 地址

--- src/config/index.js
+++ src/config/index.js
@@ -9,7 +9,7 @@ export const network = {
     LuckyPackage: '0x8b481c5af4734501ea8b6a0c3502e001dd883d3d',
     DecentralizedExchangeHotPotato: '0x46cefba4dd7512b2449e1ac4b730bfb7f77d1407',
     rpc: 'https://mainnet.infura.io/lTETGFVyQX99UKQ98BN4',
-    getPackTxApi: 'https://etherscan.io/api?module=logs&action=getLogs&fromBlock=4000&toBlock=latest&address=0x8b481c5af4734501ea8b6a0c3502e001dd883d3d&topic0=0xebc21f7ae0551d3f1d93229ed6d5faf563c7d5829ce068426ac87e428328bb23&apikey=YourApiKeyToken',
+    getPackTxApi: 'https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=4000&toBlock=latest&address=0x8b481c5af4734501ea8b6a0c3502e001dd883d3d&topic0=0xebc21f7ae0551d3f1d93229ed6d5faf563c7d5829ce068426ac87e428328bb23&apikey=YourApiKeyToken',
 
   },
   2: {
