60490befce2674d9ff22121ffe2865944d948238
Deasoso <932152571@qq.com>
2018-04-20 16:36:26 +0800

    时局界面，加入战局逻辑
    1。进入的初始界面，先看到最大势力，以及结合左边的地图颜色
    2。选中城池后可以加入战局
    
    1.玩家的武将页面
    如果没有加入战局，则先加入战局，并初始化武将和一定兵力。
    （武将头像就不显示了，把名称和几个属性列个表差不多了？）
    空降需要选择地点，如果地点没人自动占领。
    如果空降目标城池有人则自动进入战斗模式（定义为起义军）
    100个武将空投然后开始吃鸡【x
    
    2.左边城池
    各个势力不同颜色
    如果正在打架就冒红光，之类的……
    
    3.选择城池
    没有加入战局也可以查看属性，但是没有下面一堆的按钮
    
    3.1.出征（非自己的城市）
    选择任命武将……使用兵力……
    3.2 撤退	（正在战斗的非自己城市）
    3.3 调度
    选择调度兵力、武将……（自己的城市）
    3.4 移动
    选择调度武将……（自己的城市）
    3.5 搜寻 （自己的城市）
    
    未完成：
    显示玩家的武将界面
    如果点击地图外可以回到初始页面？
    左边地图应该五颜六色！
    城池主人、最大势力主人之类的都可以点，然后跳到dapdap用户界面？
    
    玩法相关：
    出征武将上限？
    对于正在战斗的城池进行支援？

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -37,7 +37,8 @@ export default {
       </div>
       <div class="tile is-parent">
         <div class="column">
-          <article class="is-child notification is-primary">
+          <!-- 城池属性界面，出现条件：点击左边一处地图 -->
+          <article class="is-child notification is-primary" v-if="selectedcity">
             <div class="columns is-mobile">
               <div class="column has-text-left subtitle">
                 城市
@@ -53,6 +54,34 @@ export default {
               </div>
             </div>
             <div class="columns is-mobile">
+              <div class="column has-text-left">
+                兵力
+              </div>
+              <div class="column has-text-right">
+                8000/20000
+              </div>
+              <div class="column has-text-left">
+                兵力恢复
+              </div>
+              <div class="column has-text-right">
+                80
+              </div>
+            </div>
+            <div class="columns is-mobile">
+              <div class="column has-text-left">
+                武将
+              </div>
+              <div class="column has-text-right">
+                5
+              </div>
+              <div class="column has-text-left">
+                总统率力
+              </div>
+              <div class="column has-text-right">
+                330
+              </div>
+            </div>
+            <!-- <div class="columns is-mobile">
               <div class="column has-text-left">
                 金
               </div>
@@ -88,13 +117,6 @@ export default {
                 10000
               </div>
             </div>
-            <div class="columns is-mobile">
-              <div class="column has-text-left">
-                武将
-              </div>
-              <div class="column has-text-right">
-                5
-              </div>
             </div>
             <div class="columns is-mobile">
               <div class="column has-text-left">
@@ -103,9 +125,47 @@ export default {
               <div class="column has-text-right">
                 1000/1000
               </div>
+            </div> -->
+          </article>
+           <!-- 时局界面，出现条件：未选择任何地图，刚刚进入地图界面 -->
+          <article class="is-child notification is-primary" v-else>
+            <div class="columns is-mobile">
+              <div class="column has-text-left subtitle">
+                最大势力
+              </div>
+              <div class="column has-text-right subtitle">
+                岛娘势力
+              </div>
+              <div class="column has-text-left subtitle">
+                创建者
+              </div>
+              <!-- 可点 -->
+              <div class="column has-text-right subtitle">
+                0x123..
+              </div>
+            </div>
+            <div class="columns is-mobile">
+              <div class="column has-text-left">
+                占领城池数
+              </div>
+              <div class="column has-text-right">
+                14
+              </div>
+              <div class="column has-text-left">
+                总武将数
+              </div>
+              <div class="column has-text-right">
+                35
+              </div>
+            </div>
+            <div class="column is-mobile">
+              选中左边城市以参与战局！
             </div>
           </article>
-          <article class="is-child notification is-warning">
+          <!-- 操作界面 -->
+          <!-- 主要作战界面，出现条件：选中地图，并且已经加入游戏，最主要的作战界面 -->
+          <div v-if="selectedcity">
+          <article class="is-child notification is-warning" v-if="isjoinedgame">
             <div class="columns is-mobile">
               <div class="column is-one-fifth"><a class="button">军事</a></div>
               <div class="column is-one-fifth"><a class="button">内政</a></div>
@@ -119,6 +179,18 @@ export default {
               <a class="button">外交</a> -->
             </div>
           </article>
+          <!-- 加入战局界面，出现条件：选中地图，但未加入战局-->
+          <article class="is-child notification is-warning" v-else>
+            <div class="columns is-mobile">
+              <div class="column"><a class="button" v-on:click="joingame">加入战局</a></div>
+              <!-- <a class="button">军事</a>
+              <a class="button">内政</a>
+              <a class="button">人事</a>
+              <a class="button">计谋</a>
+              <a class="button">外交</a> -->
+            </div>
+          </article>
+          </div>
         </div>
       </div>
     </div>
@@ -126,71 +198,78 @@ export default {
 </template>
 
 <script>
-import VeMap from 'v-charts/lib/map';
+import VeMap from "v-charts/lib/map";
 export default {
   components: { VeMap },
   created() {
     this.chartData = {
-      columns: ['位置', '势力', '兵力', '城防', '人口'],
+      columns: ["位置", "势力", "兵力", "城防", "人口"],
       rows: [
-        { 位置: '北平', 兵力: 11, 城防: 250, 势力: 999, 人口: 123 },
-        { 位置: '襄平', 人口: 1223 },
-        { 位置: '蓟', 人口: 321 },
-        { 位置: '晋阳'},
-        { 位置: '上党'},
-        { 位置: '南皮'},
-        { 位置: '兒'},
-        { 位置: '北海'},
-        { 位置: '陈留'},
-        { 位置: '洛阳'},
-        { 位置: '弘农'},
-        { 位置: '长安'},
-        { 位置: '天水'},
-        { 位置: '武威'},
-        { 位置: '酒泉'},
-        { 位置: '下堰'},
-        { 位置: '许昌'},
-        { 位置: '寿春'},
-        { 位置: '南阳'},
-        { 位置: '襄阳'},
-        { 位置: '长沙'},
-        { 位置: '桂阳'},
-        { 位置: '零陵'},
-        { 位置: '建业'},
-        { 位置: '会稽'},
-        { 位置: '建安'},
-        { 位置: '阜阳'},
-        { 位置: '柴桑'},
-        { 位置: '汉中'},
-        { 位置: '武都'},
-        { 位置: '永安'},
-        { 位置: '江州'},
-        { 位置: '成都'},
-        { 位置: '朱提'},
-        { 位置: '永昌'},
-        { 位置: '建宁'},
-        { 位置: '南海'},
-        { 位置: '苍梧'},
-        { 位置: '合浦'},
-        { 位置: '郁林'},
-        { 位置: '交趾'},
-      ],
+        { 位置: "北平", 兵力: 11, 城防: 250, 势力: 999, 人口: 123 },
+        { 位置: "襄平", 人口: 1223 },
+        { 位置: "蓟", 人口: 321 },
+        { 位置: "晋阳" },
+        { 位置: "上党" },
+        { 位置: "南皮" },
+        { 位置: "兒" },
+        { 位置: "北海" },
+        { 位置: "陈留" },
+        { 位置: "洛阳" },
+        { 位置: "弘农" },
+        { 位置: "长安" },
+        { 位置: "天水" },
+        { 位置: "武威" },
+        { 位置: "酒泉" },
+        { 位置: "下堰" },
+        { 位置: "许昌" },
+        { 位置: "寿春" },
+        { 位置: "南阳" },
+        { 位置: "襄阳" },
+        { 位置: "长沙" },
+        { 位置: "桂阳" },
+        { 位置: "零陵" },
+        { 位置: "建业" },
+        { 位置: "会稽" },
+        { 位置: "建安" },
+        { 位置: "阜阳" },
+        { 位置: "柴桑" },
+        { 位置: "汉中" },
+        { 位置: "武都" },
+        { 位置: "永安" },
+        { 位置: "江州" },
+        { 位置: "成都" },
+        { 位置: "朱提" },
+        { 位置: "永昌" },
+        { 位置: "建宁" },
+        { 位置: "南海" },
+        { 位置: "苍梧" },
+        { 位置: "合浦" },
+        { 位置: "郁林" },
+        { 位置: "交趾" }
+      ]
     };
     var theself = this;
-    this.selectedstatus = {name : "wow"};
+    //----
+    this.isjoinedgame = false;
+    this.selectedcity = false;
+    this.joingame = () => {
+      this.isjoinedgame = true;
+      theself.$forceUpdate();
+    };
+    this.selectedstatus = { name: "" };
     this.legendVisible = false;
     this.chartSettings = {
-      positionJsonLink: './static/gameGeo.json',
-      position: 'world',
+      positionJsonLink: "./static/gameGeo.json",
+      position: "world",
       beforeRegisterMap(json) {
         return json;
       },
-      selectedMode: 'single',
+      selectedMode: "single",
       zoom: 1,
       roam: false,
       itemStyle: {
         normal: {
-          borderColor: 'rgba(0, 0, 0, 0.2)',
+          borderColor: "rgba(0, 0, 0, 0.2)"
         },
         emphasis: {
           areaColor: null,
@@ -198,61 +277,71 @@ export default {
           shadowOffsetY: 0,
           shadowBlur: 20,
           borderWidth: 0,
-          shadowColor: 'rgba(0, 0, 0, 0.5)',
+          shadowColor: "rgba(0, 0, 0, 0.5)"
         },
-      },
+        fighted: {
+          areaColor: "rgba(255, 0, 0, 0.3)",
+          shadowOffsetX: 0,
+          shadowOffsetY: 0,
+          shadowBlur: 20,
+          borderWidth: 0,
+          shadowColor: "rgba(0, 0, 0, 0.2)"
+        }
+      }
     };
     this.chartExtend = {
       series: {
         showLegendSymbol: false,
         nameMap: {
-          XiangPing: '襄平',
-          BeiPing: '北平',
-          Ji: '蓟',
-          JinYang: '晋阳',
-          ShangDang: '上党',
-          NanPi: '南皮',
-          Er: '兒',
-          BeiHai: '北海',
-          ChenLiu: '陈留',
-          LuoYang: '洛阳',
-          HongNong: '弘农',
-          ChangAn: '长安',
-          TianShui: '天水',
-          WuWei: '武威',
-          JiuQuan: '酒泉',
-          XiaYan: '下堰',
-          XuChang: '许昌',
-          ShouChun: '寿春',
-          NanYang: '南阳',
-          XiangYang: '襄阳',
-          ChangSha: '长沙',
-          GuiYang: '桂阳',
-          LingLing: '零陵',
-          JianYe: '建业',
-          KuaiJi: '会稽',
-          JianAn: '建安',
-          FuYang: '阜阳',
-          ChaiSang: '柴桑',
-          HanZhong: '汉中',
-          WuDu: '武都',
-          YongAn: '永安',
-          JiangZhou: '江州',
-          ChengDu: '成都',
-          ZhuTi: '朱提',
-          YongChang: '永昌',
-          JianNing: '建宁',
-          NanHai: '南海',
-          CangWu: '苍梧',
-          HePu: '合浦',
-          YuLin: '郁林',
-          JiaoZhi: '交趾',
-        },
-      },
+          XiangPing: "襄平",
+          BeiPing: "北平",
+          Ji: "蓟",
+          JinYang: "晋阳",
+          ShangDang: "上党",
+          NanPi: "南皮",
+          Er: "兒",
+          BeiHai: "北海",
+          ChenLiu: "陈留",
+          LuoYang: "洛阳",
+          HongNong: "弘农",
+          ChangAn: "长安",
+          TianShui: "天水",
+          WuWei: "武威",
+          JiuQuan: "酒泉",
+          XiaYan: "下堰",
+          XuChang: "许昌",
+          ShouChun: "寿春",
+          NanYang: "南阳",
+          XiangYang: "襄阳",
+          ChangSha: "长沙",
+          GuiYang: "桂阳",
+          LingLing: "零陵",
+          JianYe: "建业",
+          KuaiJi: "会稽",
+          JianAn: "建安",
+          FuYang: "阜阳",
+          ChaiSang: "柴桑",
+          HanZhong: "汉中",
+          WuDu: "武都",
+          YongAn: "永安",
+          JiangZhou: "江州",
+          ChengDu: "成都",
+          ZhuTi: "朱提",
+          YongChang: "永昌",
+          JianNing: "建宁",
+          NanHai: "南海",
+          CangWu: "苍梧",
+          HePu: "合浦",
+          YuLin: "郁林",
+          JiaoZhi: "交趾"
+        }
+      }
     };
     this.chartEvents = {
-      click(e){
+      click(e) {
         theself.selectedstatus = e;
+        if (e.name != "") theself.selectedcity = true;
+        else theself.selectedcity = false;
         theself.$forceUpdate();
         console.log(e);
         console.log(this);
@@ -260,8 +349,8 @@ export default {
       },
       mouseover(e) {
         console.log(e.name);
-      },
+      }
     };
-  },
+  }
 };
 </script>
