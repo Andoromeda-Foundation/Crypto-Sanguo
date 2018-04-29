1430255fde4c10b3bcc6d60260b7c38be8631ff0
Frank Wei <frank@frankwei.xyz>
2018-04-29 15:49:09 +0800

    refactor: slimmer header with pug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,83 +1,46 @@
-<template>
-  <header class="[$route.name === 'Home' ? 'sig-home-header' : '']">
-    <nav class="navbar">
-      <div class="navbar-brand">
-        <router-link class="navbar-item"
-                     :to="{ name: 'Home'}"
-                     :style="navItemFontSize">
-          {{$t('header.nav.siteName')}}
-        </router-link>
-        <router-link class="navbar-item"
-                     :to="{ name: 'PreSale'}"
-                     :style="navItemFontSize">
-          {{$t('header.nav.preSale')}}
-        </router-link>
+<template lang="pug">
+  header(class="[$route.name === 'Home' ? 'sig-home-header' : '']")
+    nav.navbar
+      .navbar-brand
+        // 路人menu，直接用v-for循环数组，避免写入太多标签
+        router-link(v-for="menu in menus" :key="menu" class="navbar-item"
+                     :to="menu.to"
+                     :style="navItemFontSize")
+          |{{$t(menu.name)}}
 
-        <router-link v-if="!me"
-                     class="navbar-item"
-                     :to="{ name: 'Login'}"
-                     :style="navItemFontSize">
-          {{$t('header.nav.SignIn')}}
-        </router-link>
-
-        <template v-else>
-          <router-link class="navbar-item"
+        template(v-if="me")
+          router-link(class="navbar-item"
                        :to="{ name: 'Game' }"
-                     :style="navItemFontSize">
-            {{$t('header.nav.game')}}
-          </router-link>
+                     :style="navItemFontSize")
+            |{{$t('header.nav.game')}}
+          router-link(class="navbar-item"
+                       :to="{ name: 'Transaction', params:{address: me.address} }"
+                     :style="navItemFontSize")
+            |{{$t('header.nav.Transactions')}}
 
-          <router-link class="navbar-item"
-                       :to="{ name: 'Transaction', params:{address: me.address}}"
-                     :style="navItemFontSize">
-            {{$t('header.nav.Transactions')}}
-          </router-link>
+      .navbar-end
+        router-link(v-if="!me" :to="{ name: 'Login'}" class="navbar-item")
+          |{{$t('header.nav.SignIn')}}
 
-          <router-link class="navbar-item"
-                       :to="{ name: 'Explore' }"
-                     :style="navItemFontSize">
-            {{$t('header.nav.explore')}}
-          </router-link>
-
-          <router-link class="navbar-item"
-                       :to="{ name: 'HeroList' }"
-                     :style="navItemFontSize">
-            {{$t('header.nav.herolist')}}
-          </router-link>
-        </template>
-
-      </div>
-
-      <div class="navbar-end">
-        
-        <router-link v-if="!me" class="navbar-item"></router-link>
-        <template v-else>
-          <router-link class="navbar-item"
+        template(v-else)
+          router-link(class="navbar-item"
                        :to="{ name: 'User', params:{address: me.address}}"
-                     :style="navItemFontSize">
-            <img :src="getAvatar" class="avatar" />                     
-            <span class="info">
-            <p> {{getBalance}} ETH </p>
-            <p> {{getNetwork}}</p>
-            </span>          
-          </router-link>
-        </template>
+                     :style="navItemFontSize")
+            img(:src="getAvatar" class="avatar")
+            span.info
+              p|{{getBalance}} ETH
+              p|{{getNetwork}}
 
-        <div class="navbar-item">
-          <div class="field is-grouped">
-            <div class="control">
-              <div class="select">
-                <I18nSwitcher />
-              </div>
-            </div>
-          </div>
-        </div>
 
-      </div>
+        .navbar-item
+          field(class='is-grouped')
+            .control
+              .select
+                I18nSwitcher
 
-    </nav>
-  </header>
+
 </template>
+
 
 <script>
 import { getNetwork, getMe } from '@/api';
@@ -90,14 +53,14 @@ export default {
     async getAvatar() {
       const uri = await Dravatar(this.me.address);
       return uri;
-    },
-  },  
+    }
+  },
   components: {
-    I18nSwitcher,
+    I18nSwitcher
   },
   data() {
     return {
-      network: {},
+      network: {}
     };
   },
   async created() {
@@ -114,10 +77,31 @@ export default {
     this.getMe = await getMe();
   },
   computed: {
-    navItemFontSize: function () {
+    menus() {
+      return [
+        {
+          name: 'header.nav.siteName',
+          to: { name: 'Home' }
+        },
+        {
+          name: 'header.nav.preSale',
+          to: { name: 'PreSale' }
+        }, {
+          name: 'header.nav.explore',
+          to: { name: 'Explore' }
+        }, {
+          name: 'header.nav.herolist',
+          to: {
+            name: 'HeroList'
+          }
+        }
+      ];
+    },
+    navItemFontSize() {
       return {
-        'font-size': this.$store.state.locale=="en" ? 0.8+'rem' : 1.14+'rem'
-      }
+        'font-size':
+          this.$store.state.locale === 'en' ? `${0.8}rem` : `${1.14}rem`
+      };
     },
     locale: {
       get() {
@@ -126,13 +110,13 @@ export default {
         const lang = i18n.find(
           item =>
             item.locale === locale ||
-            item.aliases.some(alias => alias === locale),
+            item.aliases.some(alias => alias === locale)
         );
         return lang ? lang.locale : null;
       },
       set(value) {
         this.$store.dispatch('setLocale', value);
-      },
+      }
     },
     me() {
       return this.$store.state.me;
@@ -143,18 +127,18 @@ export default {
     },
     getNetwork() {
       return this.network.name;
-    },    
+    }
   },
   methods: {
     onCloseInfo() {
       this.isShowInfo = false;
-    },
+    }
   },
   watch: {
     locale(val) {
       this.$i18n.locale = val;
-    },
-  },
+    }
+  }
 };
 </script>
 
